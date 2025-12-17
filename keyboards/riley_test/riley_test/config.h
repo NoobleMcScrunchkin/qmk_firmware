c4e82423b0b285ddbbab3b1a1a4b9422376a4143
Riley Aslett <riley.aslett@gmail.com>
2025-12-17 17:05:05 +0000

    yeet

---
+++ keyboards/riley_test/riley_test/config.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// #define SERIAL_USART_TX_PIN B6
+
+
+#define SERIAL_USART_DRIVER SD1
+
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN B6     // USART TX pin
+#define SERIAL_USART_RX_PIN B7     // USART RX pin
+
+// // Optimise that memory babyeeee
+// #define DISABLE_LEADER
+
+// #ifndef NO_DEBUG
+// #define NO_DEBUG
+// #endif // !NO_DEBUG
+// #if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+// #define NO_PRINT
+// #endif // !NO_PRINT
+
+
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
+
+
+#define SPLIT_HAND_PIN B5
+#define MASTER_LEFT
